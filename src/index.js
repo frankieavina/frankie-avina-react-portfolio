e6250fa989d204362dcdc7fb66221f1e0e1a2b11
frankieavina <frankieavina@gmail.com>
2022-06-29 18:27:18 -0700

    13

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />    
       </BrowserRouter>           
     </StyledEngineProvider>
