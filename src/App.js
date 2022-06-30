3c5485f0b0a95078a0b9bfc09736922c2f0903f7
frankieavina <frankieavina@gmail.com>
2022-06-29 17:43:24 -0700

    12

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Loading from './components/Loading/Loading';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import DeleteIcon from '@mui/icons-material/Delete';
 
 function App(props) {
   const [isLoading, setIsLoading] = useState(true);
