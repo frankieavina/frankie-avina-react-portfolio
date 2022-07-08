e5d9ace34abb664bbbbc40c0520940dae83de913
frankieavina <frankieavina@gmail.com>
2022-07-08 11:21:38 -0700

    cont projects component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+/* eslint-disable array-callback-return */
+import React from 'react';
+import styled from 'styled-components';
+import featuredProjects from '../data/projects.json';
+import ProjectCard from '../components/Projects/ProjectCard';
+
+const ProjectsWrapper = styled.section`
+display: flex;
+width: 70%;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 100px 0;
+margin: 0 auto;
+
+@media (max-width: 768px) {
+  padding: 80px 0;
+}
+
+@media (max-width: 480px) {
+  padding: 60px 0;
+}
+  a {
+    position: relative;
+    z-index: 1;
+  }
+`
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <ProjectsWrapper>
+      <h2>Some Things I've Built</h2>
+      {featuredProjects ? 
+        featuredProjects.map((data) =>
+        <ProjectCard info={data} />
+      ):
+        <p>Loading...</p>
+      }
+
+    </ProjectsWrapper>
   )
 }
 
